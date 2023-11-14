e38fb8e2f1958a276ffde702be6306c310e463a2
Benjamin TD <benjamin.tdm@gmail.com>
2023-11-14 11:20:12 +0100

    not needed

--- next.config.mjs
+++ next.config.mjs
@@ -5,19 +5,6 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  redirects: [
-    {
-      source: '/',
-      has: [
-        {
-          type: 'host',
-          value: 'berlin.metro-memory.com',
-        },
-      ],
-      destination: 'metro-memory.com/berlin',
-      permanent: true,
-    },
-  ],
 }
 
 const withMDX = nextMDX({
