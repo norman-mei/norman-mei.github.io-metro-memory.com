a5e827bed7d7dfc989358b4d2c70ab3bc8e602e8
Benjamin TD <benjamin.tdm@gmail.com>
2023-12-21 10:32:14 +0100

    some centro changes

--- src/components/LinkPreviews.tsx
+++ src/components/LinkPreviews.tsx
@@ -2,12 +2,12 @@ import LinkPreview from './LinkPreview'
 
 const urls = [
   'https://www.theguardian.com/travel/2023/oct/31/metro-memory-and-tim-marshall-put-cartography-back-on-the-map',
+  'https://www.numerama.com/vroom/1518632-votre-mission-si-vous-lacceptez-retrouver-les-309-stations-de-metro-de-paris.html',
   'https://www.thetimes.co.uk/article/london-underground-map-game-tube-quiz-qqqgpmkps',
   'https://www.bbc.com/news/uk-england-london-67197933',
-  'https://www.numerama.com/vroom/1518632-votre-mission-si-vous-lacceptez-retrouver-les-309-stations-de-metro-de-paris.html',
   'https://www.timeout.fr/paris/actualites/ce-jeu-vous-defie-de-retrouver-le-maximum-de-stations-de-metro-de-tete-100923',
   'https://parissecret.com/jeu-stations-metro/',
-  'https://www.timeout.com/london/news/how-to-play-londons-viral-tube-metro-memory-game-102423',
+  'https://www.sopitas.com/noticias/metro-memory-juego-nombres-estaciones-metro-cdmx/',
 ]
 
 const Tweets = () => {
